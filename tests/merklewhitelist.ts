04c1d52568b127bb192f2bb51e46dd5b5b8b0710
kelvinkirima014 <kelvinkirima2@gmail.com>
2023-01-12 10:50:36 +0300

    first commit

---
+++ tests/merklewhitelist.ts
@@ -0,0 +1,16 @@
+import * as anchor from "@project-serum/anchor";
+import { Program } from "@project-serum/anchor";
+import { Merklewhitelist } from "../target/types/merklewhitelist";
+
+describe("merklewhitelist", () => {
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.AnchorProvider.env());
+
+  const program = anchor.workspace.Merklewhitelist as Program<Merklewhitelist>;
+
+  it("Is initialized!", async () => {
+    // Add your test here.
+    const tx = await program.methods.initialize().rpc();
+    console.log("Your transaction signature", tx);
+  });
+});
