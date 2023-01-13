9637d9fa0f6aacca3c5aa21a374fe335b1e1ce7f
kelvinkirima014 <kelvinkirima2@gmail.com>
2023-01-13 22:05:39 +0300

    init token mint

--- programs/merklewhitelist/src/lib.rs
+++ programs/merklewhitelist/src/lib.rs
@@ -14,9 +14,33 @@ pub mod merklewhitelist {
 
 #[derive(Accounts)]
 pub struct TokenMint<'info> {
+ /// CHECK: We're about to create this with Metaplex
     #[account(mut)]
-    pub signer: Signer<'info>,
-    token_mint: Account<'info, Mint>,
-    token_program: Program<'info, Token>,
-    system: Program<'info, System>,
+    pub metadata_account: UncheckedAccount<'info>,
+    #[account(
+        init,
+        payer = payer,
+        mint::decimals = 9,
+        mint::authority = mint_authority.key(),
+    )]
+    pub mint_account: Account<'info, Mint>,
+    #[account(
+        init, 
+        payer = payer,
+        space = 8 + 32,
+        seeds = [
+            b"mint_authority_", 
+            mint_account.key().as_ref(),
+        ],
+        bump
+    )]
+    pub mint_authority: Account<'info, MintAuthorityPda>,
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    pub rent: Sysvar<'info, Rent>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
 }
+
+#[account]
+pub struct MintAuthorityPda {}
