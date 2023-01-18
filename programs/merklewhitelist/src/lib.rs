7ba4923a311c75b0f295efbff4e88e96219faf5a
kelvinkirima014 <kelvinkirima2@gmail.com>
2023-01-18 08:50:16 +0300

    edit to redeem token

--- programs/merklewhitelist/src/lib.rs
+++ programs/merklewhitelist/src/lib.rs
@@ -1,9 +1,9 @@
 use anchor_lang::{prelude::*, solana_program::keccak};
-use anchor_spl::token::{Mint, Token, TokenAccount, MintTo};
+use anchor_spl::token::{Mint, Token, TokenAccount};
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
-fn merkle_verify(
+fn _merkle_verify(
     proof: Vec<[u8; 32]>,
     root: [u8; 32],
     leaf: [u8; 32],
@@ -26,71 +26,63 @@ fn merkle_verify(
 pub mod merklewhitelist {
     use super::*;
 
-    pub fn mint_token(ctx: Context<MintToken>) -> Result<()> {
+    pub fn redeem_token(ctx: Context<RedeemToken>, transfer_amount: u64) -> Result<()> {
         // Create the MintTo struct for our context
-        let cpi_accounts = MintTo {
-            mint: ctx.accounts.token_x.to_account_info(),
-            to: ctx.accounts.token_account.to_account_info(),
+        let transfer_instruction = anchor_spl::token::Transfer {
+            from: ctx.accounts.from.to_account_info(),
+            to: ctx.accounts.to.to_account_info(),
             authority: ctx.accounts.mint_authority.to_account_info(),
         };
         
         let cpi_program = ctx.accounts.token_program.to_account_info();
         // Create the CpiContext we need for the request
-        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+        let cpi_ctx = CpiContext::new(cpi_program, transfer_instruction);
 
-        // Execute anchor's helper function to mint tokens
-        anchor_spl::token::mint_to(cpi_ctx, 100)?;
+        // anchor's helper function to transfer tokens
+        anchor_spl::token::transfer(cpi_ctx, transfer_amount)?;
         
         Ok(())
     }
 
-    // pub fn transfer_token(ctx: Context<TransferToken>, transfer_amount: u64) -> Result<()> {
-
-    //     let transfer_instruction = anchor_spl::token::Transfer{
-    //         from: ctx.accounts.from.to_account_info(),
-    //         to: ctx.accounts.to.to_account_info(),
-    //         authority: ctx.accounts.transfer_authority.to_account_info(),
-    //     };
-         
-    //     let cpi_program = ctx.accounts.token_program.to_account_info();
-    //     // Create the Context for our Transfer request
-    //     let cpi_ctx = CpiContext::new(cpi_program, transfer_instruction);
-
-    //     // Execute anchor's helper function to transfer tokens
-    //     anchor_spl::token::transfer(cpi_ctx, transfer_amount)?;
- 
-    //     Ok(())
-    // }
 }
 
 #[derive(Accounts)]
-pub struct MintToken<'info> {
+pub struct RedeemToken<'info> {
     //the mint token
     /// CHECK: This is not dangerous since we do not read or write from this account
     #[account(mut)]
     pub token_x: Account<'info, Mint>,
     //who we want to mint the token to
-     /// CHECK: This is not dangerous since we do  not read or write from this account
     #[account(mut)]
-    pub mint_authority: UncheckedAccount<'info>,
+    pub mint_authority: Account<'info, MerkleTokenDistributor>,
+    //mint_authority ATA containing the tokens to mint
     #[account(mut)]
-    pub token_account: Account<'info, TokenAccount>,
+    pub from: Account<'info, TokenAccount>,
+    //who's redeeming the token
+    #[account(mut)]
+    pub to: Account<'info, TokenAccount>,
+    //account to pay for the mint
     #[account(mut)]
     pub payer: Signer<'info>,
-    pub rent: Sysvar<'info, Rent>,
     pub system_program: Program<'info, System>,
     pub token_program: Program<'info, Token>,
 }
 
-// #[derive(Accounts)]
-// pub struct TransferToken<'info> {
-//     #[account(mut)]
-//     pub from: Account<'info, TokenAccount>,
-//     #[account(mut)]
-//     pub to: Account<'info, TokenAccount>,
-//     pub transfer_authority: Signer<'info>,
-//     pub token_program: Program<'info, Token>,
+#[account]
+#[derive(Default)]
+pub struct MerkleTokenDistributor {
+    //base key to derive PDA
+    pub base: Pubkey,
+    //bump seed
+    pub bump: u8,
+    //256-bit Merkle root
+    pub root: [u8; 32],
+    //mint of the token
+    pub mint: Pubkey,
+    //MAX num of tokens that can be minted
+    pub max_mint_amount: u64,
+}
 
-// }
+
 
 
