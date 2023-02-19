b1510cb80e7eb565636a82499cea34be08323442
kelvinkirima014 <kelvinkirima2@gmail.com>
2023-02-19 06:51:16 +0300

    change proof to a vector of arrays

---
+++ programs/merklewhitelist/src/merkletree.rs
@@ -0,0 +1,16 @@
+use std::hash::Hasher;
+#[derive()]
+pub struct MerkleTree {
+    left_leaf: Box<dyn Hasher>,
+    right_leaf: Box<dyn Hasher>,
+
+}
+impl MerkleTree {
+    fn new(&mut self){
+    
+    }
+}
+
+fn main () {
+
+}
